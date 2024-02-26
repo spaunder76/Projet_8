3e70b3b9271d706660282d43e5fce08f329cec39
spaunder76 <arthurbousquet36@gmail.com>
2024-02-26 17:28:17 +0100

    Add files via upload

--- Projet_8/Projet_django/Projet/LITRevu/views.py
+++ Projet_8/Projet_django/Projet/LITRevu/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 
 def index(request):
@@ -10,8 +12,7 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def login_page(request):
-    return render(request, 'login.html')
-
+    return render(request, 'LITRevu/login.html')
 
 def login_view(request):
     if request.method == 'POST':
@@ -20,11 +21,35 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Rediriger l'utilisateur vers une page après la connexion réussie
-            return redirect('nom_de_la_vue')
+            return redirect('indexofLITRevu')
         else:
-            # Gérer les erreurs de connexion
-            # Par exemple, afficher un message d'erreur dans le template
-            return render(request, 'index.html', {'erreur': 'Identifiants incorrects'})
+            return redirect('indexofLITRevu')
     else:
-        return render(request, 'index.html')
+        return render(request, 'LITRevu/login.html')
+
+def registration(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        # Vérifier si les mots de passe correspondent
+        if password1 == password2:
+            # Vérifier si l'utilisateur n'existe pas déjà
+            if not User.objects.filter(username=username).exists():
+                # Créer un nouvel utilisateur
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                # Connecter l'utilisateur immédiatement après son inscription
+                login(request, user)
+                return redirect('indexofLITRevu')  # Rediriger vers la page d'accueil après l'inscription
+            else:
+                return render(request, 'LITRevu/registration.html', {'error': 'Cet utilisateur existe déjà'})
+        else:
+            return render(request, 'LITRevu/registration.html', {'error': 'Les mots de passe ne correspondent pas'})
+
+    return render(request, 'LITRevu/registration.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('indexofLITRevu')
--- Projet_8/Projet_django/Projet/Projet/urls.py
+++ Projet_8/Projet_django/Projet/Projet/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('LITRevu/', include('LITRevu.urls')),
-    path('LITRevu/login', include('LITRevu.urls'))
 ]
