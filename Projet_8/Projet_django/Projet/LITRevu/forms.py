1aae3df25841021af777dc41aef517f05c502358
spaunder76 <arthurbousquet36@gmail.com>
2024-03-11 10:29:14 +0100

    Delete Projet_8/Projet_django/Projet directory

--- Projet_8/Projet_django/Projet/LITRevu/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class TicketForm(forms.Form):
-    title = forms.CharField(max_length=100, label='Titre du ticket')
-    description = forms.CharField(widget=forms.Textarea, label='Description du ticket')
-    # Vous pouvez ajouter d'autres champs nécessaires pour votre ticket
